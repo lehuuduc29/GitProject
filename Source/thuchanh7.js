08badbab35f961ef99b043854a166926dd22d90e
lehuuduc29 <huuduc2903@gmail.com>
2021-12-20 13:53:52 +0700

    hien thi dau ra

---
+++ Source/thuchanh7.js
@@ -0,0 +1,12 @@
+function myfunction1(){
+    alert("xin chào");
+}
+function myfunction2(){
+    document.getElementById("demo").innerHTML = "HELLO EVERYONE!!";
+}
+function myfunction3(){
+    document.write("hello everybody!!!");
+}
+function myfunction4(){
+    console.log("xin chào mọi người!");
+}
